6e2567ab1cfe05e40062f5b932f4dfe509a0d8e5
CherkashinMaxim <cherkashin.maxim1@gmail.com>
2024-09-16 18:15:29 +0500

    Task3

---
+++ Tyuiu.CherkashinMM.Sprint1.Task3.V4.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.CherkashinMM.Sprint1.Task3.V4.Lib
+{
+    public class DataService
+    {
+        public double Calculate()
+        {
+            return Math.Round(2.75 * 7 + 0.5 * 7, 3);
+        }
+    }
+}
---
+++ Tyuiu.CherkashinMM.Sprint1.Task3.V4/Program.cs
@@ -0,0 +1,37 @@
+﻿using Tyuiu.CherkashinMM.Sprint1.Task3.V4.Lib;
+
+namespace Tyuiu.CherkashinMM.Sprint1.Task3.V4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнил: Черкашин М. М. | ИИПб-24-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Операторы составного присваивания                                 *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #4                                                              *");
+            Console.WriteLine("* Выполнил: Черкашин Максим Михайлович | ИИПб-24-1                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчёты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Цена тетради (руб.) -> 2.75                                             *");
+            Console.WriteLine("* Цена обложки (руб.) -> 0.5                                              *");
+            Console.WriteLine("* Количество комплектов (шт.) -> 7                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Стоимость покупки: " + ds.Calculate());
+            Console.ReadLine();
+        }
+    }
+}
